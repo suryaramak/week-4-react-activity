9091b39e418a1c35ccacf15b0499de5eb7d6a43c
suryaramak <ramakrishnan.sur@northeastern.edu>
2024-09-27 14:38:40 -0400

    final final

--- src/calculator.tsx
+++ src/calculator.tsx
@@ -37,14 +37,14 @@ export default function Calculator() {
         <CalculatorDisplay displayValue={display} />
         <div className="calculator-keypad">
           <div className="input-keys">
-            <div className="function-keys">
+            <div className="action-keys">
               <CalculatorKey className="key-clear" label={ActionKeys.CLEAR} onPress={actionKeyPressHandler} />
               <CalculatorKey className="key-sign" label={ActionKeys.SIGN_FLIP} onPress={actionKeyPressHandler} />
-              <CalculatorKey className="key-percent" label={OperatorKeys.SQRT} onPress={operatorKeyPressHandler} />
+              <CalculatorKey className="key-equals" label={ActionKeys.EQUALS} onPress={actionKeyPressHandler} />
+              <CalculatorKey className="key-decimal" label={ActionKeys.DOT} onPress={actionKeyPressHandler} />
             </div>
             <div className="digit-keys">
               <CalculatorKey className="key-0" label={NumericKeys.ZERO} onPress={numericKeyPressHandler} />
-              <CalculatorKey className="key-dot" label={ActionKeys.DOT} onPress={actionKeyPressHandler} />
               <CalculatorKey className="key-1" label={NumericKeys.ONE} onPress={numericKeyPressHandler} />
               <CalculatorKey className="key-2" label={NumericKeys.TWO} onPress={numericKeyPressHandler} />
               <CalculatorKey className="key-3" label={NumericKeys.THREE} onPress={numericKeyPressHandler} />
@@ -61,7 +61,6 @@ export default function Calculator() {
             <CalculatorKey className="key-multiply" label={OperatorKeys.MULT} onPress={operatorKeyPressHandler} />
             <CalculatorKey className="key-subtract" label={OperatorKeys.MINUS} onPress={operatorKeyPressHandler} />
             <CalculatorKey className="key-add" label={OperatorKeys.PLUS} onPress={operatorKeyPressHandler} />
-            <CalculatorKey className="key-equals" label={ActionKeys.EQUALS} onPress={actionKeyPressHandler} />
           </div>
         </div>
       </div>
